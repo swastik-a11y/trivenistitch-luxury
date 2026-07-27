354bcf67f749d8f0449ebf8e9009c24ce34e2ffb
swastik-a11y <swastik@sumaldigital.com>
2026-07-27 17:11:17 +0530

    chore: consolidate global style imports in main.tsx (remove conflicting index.css)

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,13 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "./index.css";
-
+// Consolidated global styles: use globals/variables/typography
 import "./styles/variables.css";
 import "./styles/typography.css";
 import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-
-<React.StrictMode>
-
-<App/>
-
-</React.StrictMode>
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
